4a385ed43d24b1fbe92f63535f0621b23885386d
Mohit Jain <mohittkg1997@gmail.com>
2025-09-08 21:11:01 +0530

    spring

---
+++ src/test/java/com/dqe/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dqe.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
